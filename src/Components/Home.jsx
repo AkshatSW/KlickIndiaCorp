115df042de718892cef3cbd7076dbbcfc73f3772
Akshat <akshatsrivastava200213@gmail.com>
2025-10-09 02:43:50 +0530

    debugs

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,22 +2,19 @@ import React, { useState, useEffect, useRef } from 'react'
 import { motion, AnimatePresence } from 'motion/react'
 
 // Import all your home images
-import img1 from '../assets/1.jpg'
-import img2 from '../assets/2.jpeg'
-import img3 from '../assets/3.jpg'
-import img4 from '../assets/4.jpg'
-import img5 from '../assets/5.jpg'
-import img6 from '../assets/6.webp'
+import img1 from '../assets/1.png'
+import img2 from '../assets/2.png'
+import img3 from '../assets/3.png'
+import img4 from '../assets/4.png'
+import img5 from '../assets/5.png'
+import img6 from '../assets/6.jpg'
 import img7 from '../assets/7.jpg'
-import img9 from '../assets/9.jpg'
-import img10 from '../assets/10.jpg'
-import img11 from '../assets/11.jpg'
 
 // Import About Us image
 import aboutImg from '../assets/AboutUs.png'
 
 const Home = () => {
-  const backgroundImages = [img1, img2, img3, img4, img5, img6, img7, img9, img10, img11]
+  const backgroundImages = [img1, img2, img3, img4, img5, img6, img7]
   const [currentImageIndex, setCurrentImageIndex] = useState(0)
 
   // Ref and state for About Us text height
